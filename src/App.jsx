3bf260d1c7a096bbbadd49d52829ad358ffd173d
YesicaChui <yesicachuic@gmail.com>
2024-02-12 17:29:37 -0500

    consumiendo data de json con fetch usando hook useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import ItemTarea from "./components/ItemTarea"
 
-const tareas = [
+/* const tareas = [
   {
     id:1,
     tarea:"Aprender React",
@@ -34,39 +34,58 @@ const tareas = [
     completado: true
   },
 ]
-
+ */
 function App() {
-const [inputTarea, setInputTarea] = useState("")
-const [misTareas, setMisTareas] = useState(tareas)
-const agregarTarea = ()=>{
-  console.log(inputTarea)
+  const [inputTarea, setInputTarea] = useState("")
+  const [misTareas, setMisTareas] = useState([])
+  const leerDatos = async () => {
+    const res = await fetch("data.json")
+    const data = await res.json()
+    setMisTareas(data)
+  }
 
-}
+  useEffect(() => {
+    leerDatos()
+  }, [])
+
+  const agregarTarea = () => {
+    console.log(inputTarea)
+    //creo el objeto
+    const tareaNueva = {
+      id: misTareas.length + 1,
+      tarea: inputTarea,
+      completado: false
+    }
+    //agrego el objeto en las tareas
+    //enviar a la nube y luego //almacenamiento
+    setMisTareas([...misTareas, tareaNueva])
+
+  }
 
   return (
     <>
-      <div className=" flex text-center justify-center w-[600px] h-[500px] m-auto mt-5 bg-white">
-        <div className=" w-[400px]  m-auto mt-5 bg-slate-400 rounded-md">
+      <div className=" flex text-center justify-center w-[600px] m-auto mt-5 bg-white">
+        <div className=" w-[400px]  m-auto mt-5 bg-slate-400 rounded-md mb-2">
           <h1 className="font-bold text-xl text-[#11324D] my-4">Lista de Tareas</h1>
           <div className="flex justify-around ">
             <p>Nª Tarea: {misTareas.length}</p>
-            <p>Pendientes: {misTareas.reduce((acu,elemento)=>elemento.completado?acu:acu+1,0)}</p>
+            <p>Pendientes: {misTareas.reduce((acu, elemento) => elemento.completado ? acu : acu + 1, 0)}</p>
           </div>
           <p className="text-[#11324D] font-semibold my-4">Agregar Tarea</p>
           <div className=" flex justify-between mb-4 mx-4">
-            <input type="text" 
-            placeholder="¿Qué hay que hacer?" 
-            className="rounded-[50px] px-5" 
-            value={inputTarea} 
-            onChange={(data)=>setInputTarea(data.target.value)}
+            <input type="text"
+              placeholder="¿Qué hay que hacer?"
+              className="rounded-[50px] px-5"
+              value={inputTarea}
+              onChange={(data) => setInputTarea(data.target.value)}
             />
             <button onClick={agregarTarea} className="rounded-lg bg-[#11324D] text-white font-semibold p-[4px] text-center px-4">Agregar</button>
           </div>
           {
-            misTareas.map((tarea,index)=>(
-             <ItemTarea key={tarea.id} textoTarea={tarea.tarea} estaCompletada={tarea.completado}/>
+            misTareas.map((tarea, index) => (
+              <ItemTarea key={tarea.id} textoTarea={tarea.tarea} estaCompletada={tarea.completado} />
             ))
-          }      
+          }
         </div>
       </div>
     </>
